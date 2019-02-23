10dee0a6a0524ca882d6f5cad6c448530dcf5746
KUNAL FARMAH <kunalfarmah98@gmail.com>
2019-02-23 20:22:07 +0530

    final code

--- Greedy/deepakAndHisJourney.cpp
+++ Greedy/deepakAndHisJourney.cpp
@@ -1,89 +1,45 @@
-
-//KUNAL FARMAH
-//@kunalfarmh98@gmail.com
-
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
+typedef long long ll;
+int main() {
 
+    ll t;
+    cin>>t;
 
-int min_index(int a[], int b,int e){
-	int min_index=0;
-	int min = INT_MAX;
+    while(t--){
+        ll n;
+        cin>>n;
 
-	for(int i=b;i<e;i++){
-		if(a[i]<min){
-			min_index = i;
-			min = a[i];
-		}
-	}
+        ll l[n],  c[n], mincost[n];
 
-	return min_index;
-}
+        for(int i=0 ;i<n; i++){
 
-int minCost(int n, int L[], int C[]){
+            cin>>c[i];
+            //he has to fill up at first station
+            if(i==0){
+            mincost[i]=c[i];
+            }
 
-	// he has to reach the first checkpoint
-	int cost =0;
+            // at each step taking the min of previous value and current value
 
-// finding the checkpoint with least cost to fill the tank
-	int minCostIndex = min_index(C,0,n);
+            else{
+                mincost[i] = min(c[i],mincost[i-1]);
+            }
+        }
 
-// find the min cost checkpoint in the way to reach the min checkpoint
-	int minCostmidIndex = min_index(C,0,minCostIndex);
+         for(int i=0 ;i<n; i++){
+             cin>>l[i];
+         }
 
-// reach that middle checkpoint from the first checkpoint
-	for(int i=0;i<minCostmidIndex;i++){
-		cost+=C[i]*L[i];
-	}
-// calculate the required amount of petrol to reach the min checkpoint
-	int required = 0;
-	for(int i=minCostmidIndex; i<minCostIndex; i++){
-		required+=L[i];
-	}
-// fill the tank completely at the minimun cost middle checkpoint;
-	cost+=required*C[minCostmidIndex];
+         ll cost=0;
 
-	// calculate the required amount of petrol to reach the end from the min checkpoint
-	required = 0;
-	for(int i=minCostIndex; i<n; i++){
-		required+=L[i];
-	}
+         for(int i=0; i<n; i++){
+             // calculating cost of filling up petrol at each min station
+             cost += l[i]*mincost[i];
+         }
 
-	cost+=required*C[minCostIndex];
-
-	return cost;
-
-
-
-}
-
-int main(){
-
-	int t;
-	cin>>t;
-
-	while(t--){
-		int n;
-		cin>>n;
-
-		int L[n],C[n];
-
-
-
-		for(int i=0; i<n; i++){
-			cin>>C[i];
-		}
-
-		for(int i=0; i<n; i++){
-			cin>>L[i];
-		}
-
-
-		cout<<minCost(n,L,C)<<endl;
-
-	}
-
-
-
-	return 0;
+         cout<<cost<<endl;
+        
+    }
+    
 }
