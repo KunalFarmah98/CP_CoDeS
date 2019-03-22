76b4a5a297e5ecefabd7debcd1f5a7806a4e9259
KUNAL FARMAH <kunalfarmah98@gmail.com>
2019-03-22 23:02:31 +0530

    Add files via upload

---
+++ DP/diwali_puzze(3D DP).cpp
@@ -0,0 +1,82 @@
+
+//KUNAL FARMAH
+//@kunalfarmh98@gmail.com
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+
+ll MOD = 1000003;
+ll n,k;
+ll dp[101][5][101];
+
+
+ll  diw_puzl(ll i, ll prev, ll cnt){
+
+// if we reach last element
+	if(i==n){
+		// if we have got ans as k, we have found 1 way to do it
+		if(cnt==k)
+			return 1;
+		
+			return 0;
+	}
+
+	
+    
+
+    if(dp[i][prev][cnt]!=-1)
+    	return dp[i][prev][cnt];
+
+    ll ans = 0;
+	
+	// if we place 1 in i
+	
+	if(prev==1){
+		// if prev element was 1, the contribution increases by 1
+       ans =  ans+diw_puzl(i+1,1,cnt+1);
+       
+       // i also becomes prev for i+1
+	}
+	else{
+		// if prev was 0, contribution remains same
+		ans = ans+diw_puzl(i+1,1,cnt);
+		
+    }
+
+    ans%=MOD;
+
+    // if we place 0
+    // contribution remains same no matter what prev was
+
+     ans = ans + diw_puzl(i+1,0,cnt);
+     ans%=MOD;
+
+     return dp[i][prev][cnt] = ans;
+
+
+
+}
+
+int main(){
+	
+   ll t;
+   cin>>t;
+
+   while(t--){
+
+   	memset(dp,-1,sizeof(dp));
+  
+   	cin>>n>>k;
+
+   	// we return sum of both cases 
+   	// when we put 1 + when we put 0 as we are free to assume any configuration
+
+   	cout<<(diw_puzl(1,0,0)+diw_puzl(1,1,0)%MOD)<<endl;
+   }
+
+
+	return 0;
+}
